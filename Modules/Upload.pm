9421bdfa18f139723a7e3f8ebed748a5371c9bbf
Chad Laing <chadlaing@gmail.com>
2015-06-18 11:56:57 -0600

    Removed bareword filehandle.

--- Modules/Upload.pm
+++ Modules/Upload.pm
@@ -305,10 +305,10 @@ sub upload_genome : Runmode {
 	my $tmpfile = $file_path . "genodo-form-params-$tracking_id.txt";
 	# chmod 0644, $tmpfile
 	
-	open(OUT,">$tmpfile");
-	print OUT Data::Dumper->Dump([\%genome_params, \%upload_params], ['contig_collection_properties', 'upload_parameters']);
-	close OUT;
-	
+	opem $outFH, '>', $tmpfile or die "Could not open $tmpfile\n";
+	$outFH->print(Data::Dumper->Dump([\%genome_params, \%upload_params], ['contig_collection_properties', 'upload_parameters']));
+	$outFH->close();
+
 	# Save arguments to main config file
 	my $optFile = $file_path . "genodo-options-$tracking_id.cfg";
 	my $opt = new Config::Simple(syntax => 'ini') or croak "Cannot create config object " . Config::Simple->error();
