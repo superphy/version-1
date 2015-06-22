57661b825294250befe58ac7f9aa69509879de50
Nicolas Tremblay <ns.tremblay@gmail.com>
2015-06-22 15:17:36 -0600

    Update update_master_branch.pl

--- App/Pages/update_master_branch.pl
+++ App/Pages/update_master_branch.pl
@@ -2,6 +2,22 @@
 
 use strict;
 use warnings;
+use Git::Hook::PostReceive;
 
-system('git pull origin master');
+my $payload = Git::Hook::PostReceive->new->read_stdin( <STDIN> );
 
+
+#check if there is a push and make sure that this push is from master
+if(exists $payload->{ref}){
+	if($payload->{ref} eq "refs/heads/master"){
+		system('git pull origin master');
+	}	
+}
+
+#check to see if the hook involves a pull request
+if(exists $payload->{pull_request}){
+	#if the pull request is closed, this signals that merges were made and then pull
+	if($payload->{pull_request}->{merged} eq 'true'){
+		system('git pull origin master');
+	}
+}
