b9f1984f78dd8380ffb06e4f48fd09e662896d1b
Chad Laing <chadlaing@gmail.com>
2015-08-04 13:42:54 -0600

    GitHub POST handling moved to Update.pm

--- Modules/Update.pm
+++ Modules/Update.pm
@@ -10,22 +10,34 @@ $ENV{PATH_INFO} =~ s/^$ENV{DOCUMENT_ROOT}// if defined $ENV{PATH_INFO};
 use strict;
 use warnings;
 use FindBin;
+use JSON;
 use lib "$FindBin::Bin/..";
 use parent 'CGI::Application';
+
 use CGI::Application::Plugin::AutoRunmode;
 use File::Basename;
 
 #get script location via File::Basename
 my $SCRIPT_LOCATION = dirname(__FILE__);
 
-
 sub update : StartRunmode{
     my $self = shift;
-    my $payload = $self->param('payload');
+    my $payload = $self->query->param('POSTDATA');
 
-	system($SCRIPT_LOCATION . '/../App/Pages/update_master_branch.pl');
-    return $payload;
+    my $inJSON = from_json($payload);
+
+    #"true" values in JSON converts to 1 in perl data structure
+    if(exists $inJSON->{pull_request} &&
+    	exists $inJSON->{pull_request}->{merged} &&
+        $inJSON->{pull_request}->{merged} eq 1){
+    	system('git pull origin master');
+
+    	#response given back to the hook
+    	return("Pulled new version");
+    }
+    else{
+    	return("POST request did not trigger a pull");
+    }
 }
-
 
 1;
